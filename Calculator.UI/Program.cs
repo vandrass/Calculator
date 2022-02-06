958edf7c37a58a50d933b50172c167e6a30e9022
Ivan Goncharov <vandrass@gmail.com>
2022-02-06 11:19:18 +0200

    Add project files.

---
+++ Calculator.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator.UI
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
